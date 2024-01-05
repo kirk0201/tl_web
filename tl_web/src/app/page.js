0d450574387213baaed5d7371eaaa495d7bc31d1
김재현 <b231006@koscom.co.kr>
2024-01-05 17:19:12 +0900

    RootLayout 수정

--- tl_web/src/app/page.js
+++ tl_web/src/app/page.js
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main className="flex min-h-screen justify-between p-24">Home!</main>;
+  return (
+    <main className="flex min-h-screen justify-between p-24">
+      <Link href="/button1">Home!</Link>
+    </main>
+  );
 }
--- tl_web/src/components/navmenu.js
+++ tl_web/src/components/navmenu.js
@@ -1,3 +1,28 @@
+import NavButton from "./navbutton";
+
 export default function NavMenu({ children }) {
-  return <nav className="w-full h-[60px] flex">{children}</nav>;
+  const link = [
+    { name: "홈", endPoint: "/" },
+    { name: "Button1", endPoint: "/button1" },
+    { name: "Button2", endPoint: "/button2" },
+    { name: "Button3", endPoint: "/button3" },
+  ];
+  return (
+    <nav className="w-full h-[60px] flex">
+      {link.map((comp) => {
+        return <NavButton key={comp.name} pageInfo={comp}></NavButton>;
+      })}
+      {/* <NavButton pageInfo={{ name: "홈", endPoint: "/" }}></NavButton>
+      <NavButton
+        pageInfo={{ name: "Button1", endPoint: "/button1" }}
+      ></NavButton>
+      <NavButton
+        pageInfo={{ name: "Button2", endPoint: "/button2" }}
+      ></NavButton>
+      <NavButton
+        pageInfo={{ name: "Button3", endPoint: "/button3" }}
+      ></NavButton> */}
+      {children}
+    </nav>
+  );
 }
